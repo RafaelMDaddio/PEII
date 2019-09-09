03b8a0b66d680f375b56e1f38a4e63f5df6c23c8
RafaelMDaddio <55092816+RafaelMDaddio@users.noreply.github.com>
2019-09-09 10:06:01 -0300

    Add files via upload

---
+++ exMatriz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int tam,i,j, *soma;
+    printf("Digite o tamanho da matriz:\n");
+    scanf("%d",&tam);
+
+    int p[tam][tam];
+
+	printf("\nDigite os elementos da matriz:\n");
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			scanf("%d", &p[i][j]);	
+		}		
+	}
+	
+    printf("\n Exibindo a matriz: \n");
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			printf("%d ", p[i][j]);	
+		}
+		printf("\n");		
+	}
+	
+	soma = somaLinha(tam,p); //implementar
+    printf("\nExibindo a soma das linhas:\n");
+	for(i=0;i<3;i++){
+		printf("Linha %d = %d\n",i,soma[i]);
+	}
+	
+}
